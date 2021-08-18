3769c7e8a686679326775955b38922dacd3068d5
AllRox <alexandre@allrox.com.br>
2021-08-18 13:51:05 -0300

    Alterações no enviar.php

--- enviar.php
+++ enviar.php
@@ -1,6 +1,5 @@
 <?php 
 	
-	// Pegamos os dados vindo do formulário
 	$nome = $_POST['nome'];
 	$email = trim($_POST['email']);
 	$telefone = $_POST['telefone'];
@@ -10,8 +9,7 @@
 	$aceite = $_POST['aceite'];
         $assunto = "Mensagem recebida do site";
 
-         // Aqui iremos montar o conteúdo que será enviado em seu email
-		$body = '
+        $body = '
 			<b>Mensagem enviada via Formulário</b>
 			<br>
 			<b>Nome:</b> '.$nome.'<br>
@@ -25,15 +23,12 @@
 			Mensagem enviada em: '.date('d/m/Y H:is').'
 		';
 
-		// Vamos o header do email conforme determina a RFC 822
-		// O Return-Path deve ser o mesmo do From
 		$headers = "MIME-Version: 1.1\r\n";
 		$headers .= "Content-type: text/html; charset=utf-8\r\n";
 		$headers .= "From: alexandre@allrox.com.br\r\n";
 		$headers .= "Return-Path: alexandre@allrox.com.br\r\n";
 		$envio = mail("alexandre@allrox.com.br", $assunto, $body, $headers); 
 
-		// Aqui iremos validar o envio com if
 		if($envio){
 			echo "<script>location.href='https://allrox.com.br/lab/alura/sucesso.html'</script>";
 		} else {
